e95d6da01231337f20532eedc870fbb68f720ea2
Matthieu <113586696+MatthieuGuer@users.noreply.github.com>
2022-09-20 14:29:06 +0200

    Upload du plugin
    Je savais pas comment faire un commit propre, donc j'ai uploadé à la main...

---
+++ pymodaq_plugins_uniblitz/src/pymodaq_plugins_uniblitz/daq_move_plugins/daq_move_VLM1.py
@@ -0,0 +1,211 @@
+from pymodaq.daq_move.utility_classes import DAQ_Move_base  # base class
+from pymodaq.daq_move.utility_classes import comon_parameters, main  # common set of parameters for all actuators
+from pymodaq.daq_utils.daq_utils import ThreadCommand, getLineInfo  # object used to send info back to the main thread
+from easydict import EasyDict as edict  # type of dict
+import serial
+from serial.tools import list_ports
+
+class DAQ_Move_VLM1(DAQ_Move_base):
+    """
+        Wrapper object to access the Mock fonctionnalities, similar wrapper for all controllers.
+
+        =============== ==============
+        **Attributes**    **Type**
+        *params*          dictionnary
+        =============== ==============
+    """
+    _controller_units = 'whatever'
+    is_multiaxes = False  # set to True if this plugin is controlled for a multiaxis controller (with a unique communication link)
+    stage_names = []  # "list of strings of the multiaxes
+
+    COMports = [COMport.device for COMport in list_ports.comports()]
+    isOpened = False
+    if len(COMports) > 0:
+        COMport = COMports[0]
+    else:
+        COMport = None
+
+    params = [   
+        {
+            'title': 'COM Port:',
+            'name': 'COM_port',
+            'type': 'list',
+            'limits': COMports,
+            'value': COMport
+        },
+        {
+            'title': 'isOpened',
+            'name': 'isOpened',
+            'type': 'bool',
+            'value': isOpened
+        },
+        ## TODO for your custom plugin
+                 # elements to be added here as dicts in order to control your custom stage
+                 ############
+                 {'title': 'MultiAxes:', 'name': 'multiaxes', 'type': 'group', 'visible': is_multiaxes, 'children': [
+                     {'title': 'is Multiaxes:', 'name': 'ismultiaxes', 'type': 'bool', 'value': is_multiaxes,
+                      'default': False},
+                     {'title': 'Status:', 'name': 'multi_status', 'type': 'list', 'value': 'Master',
+                      'limits': ['Master', 'Slave']},
+                     {'title': 'Axis:', 'name': 'axis', 'type': 'list', 'limits': stage_names},
+
+                 ]}] + comon_parameters
+    print('parametres resolus')
+
+    def __init__(self, parent=None, params_state=None):
+        """
+            Initialize the the class
+
+            ============== ================================================ ==========================================================================================
+            **Parameters**  **Type**                                         **Description**
+
+            *parent*        Caller object of this plugin                    see DAQ_Move_main.DAQ_Move_stage
+            *params_state*  list of dicts                                   saved state of the plugins parameters list
+            ============== ================================================ ==========================================================================================
+
+        """
+        print('init')
+        super().__init__(parent, params_state)
+        self.controller = None
+
+
+
+    def check_position(self):
+        """Get the current position from the hardware with scaling conversion.
+
+        Returns
+        -------
+        float: The position obtained after scaling conversion.
+        """
+
+        if self.isOpened:
+            pos = 1.0
+        else:
+            pos = 0.0
+        self.emit_status(ThreadCommand('check_position',[pos]))
+        return pos
+
+
+    def close(self):
+        """
+        Terminate the communication protocol
+        """
+
+        self.controller.close()
+
+
+    def commit_settings(self, param):
+        """
+            | Activate any parameter changes on the PI_GCS2 hardware.
+            |
+            | Called after a param_tree_changed signal from DAQ_Move_main.
+
+        """
+
+        if param.name() == "COMPort":
+            self.close()
+            self.COMport = param.value()
+        elif param.name() == 'isOpened':
+            self.isOpened = param.value()
+
+        else:
+            pass
+
+
+    def ini_stage(self, controller=None):
+        """Actuator communication initialization
+
+        Parameters
+        ----------
+        controller: (object) custom object of a PyMoDAQ plugin (Slave case). None if only one actuator by controller (Master case)
+
+        Returns
+        -------
+        self.status (edict): with initialization status: three fields:
+            * info (str)
+            * controller (object) initialized controller
+            *initialized: (bool): False if initialization failed otherwise True
+        """
+
+        print('Tentative d\'initialisation')
+        try:
+            # initialize the stage and its controller status
+            # controller is an object that may be passed to other instances of DAQ_Move_Mock in case
+            # of one controller controlling multiactuators (or detector)
+
+            self.status.update(edict(info="", controller=None, initialized=False))
+
+            # check whether this stage is controlled by a multiaxe controller (to be defined for each plugin)
+            # if multiaxes then init the controller here if Master state otherwise use external controller
+            if self.settings.child('multiaxes', 'ismultiaxes').value() and self.settings.child('multiaxes',
+                                   'multi_status').value() == "Slave":
+                if controller is None:
+                    raise Exception('no controller has been defined externally while this axe is a slave one')
+                else:
+                    self.controller = controller
+            else:
+                self.controller = serial.Serial(self.COMport, baudrate=9600)
+
+
+            print('port ouvert')
+            self.status.info = "Port ouvert"
+            self.status.controller = self.controller
+            self.status.initialized = True
+            return self.status
+
+        except Exception as e:
+            self.emit_status(ThreadCommand('Update_Status',[getLineInfo()+ str(e),'log']))
+            self.status.info=getLineInfo()+ str(e)
+            self.status.initialized=False
+            return self.status
+
+
+    def move_Abs(self, position):
+        """ Move the actuator to the absolute target defined by position
+
+        Parameters
+        ----------
+        position: (flaot) value of the absolute target positioning
+        """
+        
+        if position >= 1:
+            position = 1
+        else:
+            position = 0
+
+
+        self.controller.write([b'A', b'@'][position])
+        self.isOpened = not self.isOpened
+
+    def move_Rel(self, position):
+        """ Move the actuator to the relative target actuator value defined by position
+
+        Parameters
+        ----------
+        position: (flaot) value of the relative target positioning
+        """
+        position = self.check_bound(self.current_position+position)-self.current_position
+        self.target_position = position + self.current_position
+
+        if position > 0:
+            self.move_Abs(1)
+        else:
+            self.move_Abs(0)
+
+    def move_Home(self):
+        """
+          Send the update status thread command.
+            See Also
+            --------
+            daq_utils.ThreadCommand
+        """
+        self.move_Abs(0)
+
+
+if __name__ == '__main__':
+    # main(__file__)
+    test = DAQ_Move_VLM1()
+    print(test.params)
+    test.ini_stage()
+    test.move_Abs(0)
+    test.close()
